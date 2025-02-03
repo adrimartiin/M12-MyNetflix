9de095e59ce9444f94a8b606cd4c4dd96b61157d
RobertoNobleMaestro <10007851.joan23@fje.edu>
2025-02-03 17:35:09 +0100

    validaciones forms

--- login.php
+++ login.php
@@ -16,18 +16,19 @@
             <div class="col-6 justify-content-center align-items-center d-flex">
                 <div style="width: 80%;">
                     <h3 class="text-center mb-4">Iniciar Sesión</h3>
-                    <form action="" method="post">
+                    <form action="" method="post" id="formulario-login">
                         <div class="mb-3">
                             <label for="usuario" class="form-label">Usuario:</label>
-                            <input type="text" id="usuario" name="usuario" class="form-control">
+                            <input type="text" id="usuario-login" name="usuario-login" class="form-control">
                         </div>
                         <div class="mb-3">
                             <label for="password" class="form-label">Contraseña:</label>
-                            <input type="password" id="password" name="password" class="form-control">
+                            <input type="password" id="password-login" name="password-login" class="form-control">
                         </div>
                         <div class="d-grid">
                             <button type="submit" class="btn-login" name="login" id="login">Login</button>
                         </div>
+                        <p id="error-form" class="error"></p>
                         <!-- Enlace para crear cuenta -->
                         <a href="registrarse.php" class="link-crear-cuenta">Crear cuenta</a>
                     </form>
@@ -35,6 +36,7 @@
             </div>
         </div>
     </div>
+    <script src="./js/login.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
