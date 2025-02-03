50947d0d27698981959a1b45a32c08937b13277d
adrimartin6 <adrianmartincalvet@gmail.com>
2025-02-03 17:55:18 +0100

    Archivo gestión de usuarios en curso

--- private/gest_users.php
+++ private/gest_users.php
@@ -40,13 +40,36 @@
             background-color: #F8F8F8;
             color: #333;
         }
+
+        table {
+            width: 100%;
+            border-collapse: collapse;
+            text-align: center;
+        }
+
+        .table_head {
+            background-color: #F1F1F1;
+        }
+
+        .table_body {
+            background-color: #DB202C;
+        }
+
+        .form-check-input:checked {
+            background-color: #000 !important;
+            border-color: #000 !important;
+        }
+
+        .form-check-input:focus {
+            box-shadow: none !important;
+        }
     </style>
 </head>
 
 <body>
     <nav class="navbar navbar-expand-lg">
         <div class="container-fluid">
-            <a class="navbar-brand text-white" href="#"><img src="../img/logo.png"></a>
+            <a class="navbar-brand text-white" href="./indexAdmin.php"><img src="../img/logo.png"></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
@@ -64,13 +87,14 @@
                 <th>Usuario</th>
                 <th>Email</th>
                 <th>Fecha de Registro</th>
+                <th>Rol</th>
                 <th>Acciones
                     (Habilitar/Deshabilitar)
                 </th>
             </tr>
         </thead>
         <tbody class="table_body">
-          
+            <?php include '../procesos/proc_users.php'; ?>
         </tbody>
     </table>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
